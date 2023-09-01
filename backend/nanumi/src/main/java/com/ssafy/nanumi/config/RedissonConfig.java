b5e895505c59ac0bde8a94b5b23c0e330b4f9fd9
woozink <k0109974@naver.com>
2023-09-01 22:09:00 +0900

    서버 정상화 성공

--- backend/nanumi/src/main/java/com/ssafy/nanumi/config/RedissonConfig.java
+++ backend/nanumi/src/main/java/com/ssafy/nanumi/config/RedissonConfig.java
@@ -32,8 +32,8 @@ public class RedissonConfig {
     @Value("${spring.redis.port}")
     private int redisPort;
 
-    @Value("${spring.redis.password}")
-    private String redisPassword;
+//    @Value("${spring.redis.password}")
+//    private String redisPassword;
 
     private static final String REDISSON_HOST_PREFIX = "redis://";
 
@@ -42,30 +42,30 @@ public class RedissonConfig {
         RedissonClient redisson = null;
         Config config = new Config();
         config.useSingleServer()
-                .setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort)
-                .setPassword(redisPassword);
+                .setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort);
+//                .setPassword(redisPassword);
         redisson = Redisson.create(config);
         return redisson;
     }
-    @Bean
-    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier
-            , ServletEndpointsSupplier servletEndpointsSupplier
-            , ControllerEndpointsSupplier controllerEndpointsSupplier
-            , EndpointMediaTypes endpointMediaTypes
-            , CorsEndpointProperties corsProperties
-            , WebEndpointProperties webEndpointProperties
-            , org.springframework.core.env.Environment environment) {
-        List<ExposableEndpoint<?>> allEndpoints = new ArrayList<>();
-        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
-        allEndpoints.addAll(webEndpoints);
-        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
-        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
-        String basePath = webEndpointProperties.getBasePath();
-        EndpointMapping endpointMapping = new EndpointMapping(basePath);
-        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment, basePath);
-        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes, corsProperties.toCorsConfiguration()
-                , new EndpointLinksResolver(allEndpoints, basePath), shouldRegisterLinksMapping, null);
-    }
+//    @Bean
+//    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier
+//            , ServletEndpointsSupplier servletEndpointsSupplier
+//            , ControllerEndpointsSupplier controllerEndpointsSupplier
+//            , EndpointMediaTypes endpointMediaTypes
+//            , CorsEndpointProperties corsProperties
+//            , WebEndpointProperties webEndpointProperties
+//            , org.springframework.core.env.Environment environment) {
+//        List<ExposableEndpoint<?>> allEndpoints = new ArrayList<>();
+//        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+//        allEndpoints.addAll(webEndpoints);
+//        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+//        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+//        String basePath = webEndpointProperties.getBasePath();
+//        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+//        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment, basePath);
+//        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes, corsProperties.toCorsConfiguration()
+//                , new EndpointLinksResolver(allEndpoints, basePath), shouldRegisterLinksMapping, null);
+//    }
 
     private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment, String basePath) {
         return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
